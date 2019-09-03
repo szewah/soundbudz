9d664eac7bf7c6780c92b1083d1359a61a2c0f1d
szewah <swgrleong@gmail.com>
2019-09-03 12:55:08 -0400

    settig up navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,2 +1,25 @@
 import React, {Component} from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import Logo from '../../logo/blacklogo.png';
+
+class Navbar extends Component {
+    state = {
+        navExpanded: false
+    }
+
+    setNavExpanded = (expanded) => {
+        this.setState({navExpanded: expanded})
+    }
+
+    render() {
+        return (
+            <Navbar bg="dark" expand="lg">
+                <Nav>
+
+                </Nav>
+
+            </Navbar>
+        )
+    }
+}
