da1ca81d61ac13b25e84837bf48f637982d9db27
szewah <swgrleong@gmail.com>
2019-09-03 14:10:04 -0400

    added react router

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import Logo from '../../logo/blacklogo.png';
+import '../navbar/style.css'
 
 class Navigation extends Component {
     state = {
@@ -13,29 +14,17 @@ class Navigation extends Component {
             <Navbar.Brand href="#home">
                 <img
                     src={Logo}
-                    width="30"
-                    height="30"
                     className="d-inline-block align-top"
-                    alt="React Bootstrap logo"
+                    alt="Soundbudz logo"
                 />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                <Nav className="mr-sm-2">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">About</Nav.Link>
+                    <Nav.Link href="#link">Login</Nav.Link>
                 </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
-                </Form>
             </Navbar.Collapse>
             </Navbar>
         )
