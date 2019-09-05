dda5a447ceb702ed54910f8415a381383af5c160
szewah <swgrleong@gmail.com>
2019-09-05 10:37:09 -0400

    login page

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,21 +1,38 @@
 import React, {Component} from 'react';
 import {Form, Button} from 'react-bootstrap';
 import './style.css';
+import { runInThisContext } from 'vm';
 
 class Login extends Component {
     state = {
+        email: '',
+        password: ''
+    } 
 
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        const User = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        alert('A name was submitted: ' + this.state.email + this.state.password);
+        
     }
 
     render() {
         return (
             <div className="login-email-container">
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.Group>
-                        <Form.Control type="email" name="email" placeholder="Email"></Form.Control>
+                        <Form.Control name="email" type="email" value={this.state.email} placeholder="Email" onChange={this.handleChange}></Form.Control>
                     </Form.Group>
                     <Form.Group>
-                        <Form.Control type="password" name="password" placeholder="Password "></Form.Control>
+                        <Form.Control name="password" type="password" value={this.state.password} placeholder="Password" onChange={this.handleChange}></Form.Control>
                     </Form.Group>
                     <div className="btn-container">
                         <Button type="submit">Submit</Button>
