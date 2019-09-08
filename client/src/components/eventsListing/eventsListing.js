3c9ee170a9e12479e009f7aa8b5c0a1694e61d0e
szewah <swgrleong@gmail.com>
2019-09-08 07:08:34 -0400

    accessing properties of withRouter

--- client/src/components/eventsListing/eventsListing.js
+++ client/src/components/eventsListing/eventsListing.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Redirect, withRouter} from 'react-router-dom';
+//turn string into an object
 
 class EventsListing extends Component {
     state ={
@@ -6,11 +8,13 @@ class EventsListing extends Component {
     }
 
     render() {
-        //turn string into an object
+        const {match, location,history} = this.props;
+        const events_data = JSON.parse(this.props.location.state.events_data);
         // const events_data = JSON.parse(this.props.location.state.events_data);
-        // console.log(`These are the events: ${events_data}`);
         return(
             <h2>Another hello</h2>
+            
+            // console.log(`These are the events: ${events_data}`);
         )
     }
 }
