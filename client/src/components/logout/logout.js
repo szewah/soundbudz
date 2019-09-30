4a64ae256231be93d4375d6708891fdbd3fb30f7
szewah <swgrleong@gmail.com>
2019-09-30 13:02:49 -0400

    working on logout

--- client/src/components/logout/logout.js
+++ client/src/components/logout/logout.js
@@ -1,4 +1,19 @@
 import React, {Component} from 'react';
-import {Form, Button, Container} from 'react-bootstrap';
+import {Form, Button, Container, Nav} from 'react-bootstrap';
+import {Redirect} from 'react-router-dom';
 import './style.css';
 
+class Logout extends Component {
+
+    logoutClick = (event) => {
+        event.preventDefault();
+        localStorage.removeItem('token');
+    }
+
+    render() {
+        return 
+        <Nav.Link href="" onClick={this.logoutClick}>Logout</Nav.Link>
+    }
+}
+
+export default Logout;
