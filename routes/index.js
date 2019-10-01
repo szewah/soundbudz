653f8520a163828b392a270428797d62cef61fd0
szewah <swgrleong@gmail.com>
2019-10-01 10:49:06 -0400

    index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,16 @@
-const register = require('./register');
-const login = require('./login');
+const registerAPI = require('./register');
+const loginAPI = require('./login');
+const path = require('path');
+
+//API routes
+app.use('/registration', registerAPI);
+app.use('/login', loginAPI);
+
+//If no API routes are hit, send the front of the react app
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+})
+
+module.exports = app;
+
+
