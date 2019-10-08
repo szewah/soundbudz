f4d4e667ed3ee0004ff59dae8639731db6650142
szewah <swgrleong@gmail.com>
2019-10-08 09:00:26 -0400

    adding login authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,39 +3,38 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-// router.post('/new', userController.create);
 
 //Registering new user
 router.post('/new', (req, res) => {
-            db.User.findOne({where:{email:req.body.email}})
-            .then(user => {
-                if(user) {
-                    res.status(400).json({email: "Email is already in use"});
-                }
-                else {
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(req.body.password, salt, (err, hash) => {
-                            db.User.create({
-                                firstName: req.body.firstName,
-                                lastName: req.body.lastName,
-                                email: req.body.email,
-                                password: hash
-                            })
-                            .then(user => res.json(user))
-                            .catch(err => res.status(400).json(err));
-                        })
+    db.User.findOne({where:{email:req.body.email}})
+    .then(user => {
+        if(user) {
+            res.status(400).json({email: "Email is already in use"});
+        }
+        else {
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(req.body.password, salt, (err, hash) => {
+                    db.User.create({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: hash
                     })
-                }
+                    .then(user => res.json(user))
+                    .catch(err => res.status(400).json({error: "There was an error"}));
+                })
             })
-
+        }
+    })
 })
 
 
-
-
-
 //Login
+router.post('/login', (req, res) => {
+    
+})
 
 
 // Find all users
