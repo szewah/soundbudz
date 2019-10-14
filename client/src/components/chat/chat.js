ab4d82597a9561474c94f7603b28874816976624
szewah <swgrleong@gmail.com>
2019-10-14 13:40:21 -0400

    added chat room

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card, Button} from 'react-bootstrap';
+import {Card, Form, Button} from 'react-bootstrap';
 
 
 class Chat extends Component {
@@ -8,16 +8,46 @@ class Chat extends Component {
         messages: []
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+
+    sendMessage = (event) => {
+        event.preventDefault();
+        alert('I was clicked');
+        const message = this.state.message;
+        this.setState({message});       
+        console.log(message);
+    }
+
+
     render() {
         return(
-            <div className="container">
-                <Card 
-                type="text" 
-                name="messageContent" 
-                placeholder='Mesage'
-                value={this.state.message} 
-                />
-                <Button>Send</Button>
+            <div id="chatContainerForm" className="container">
+                {/* chat area can only be returned if the messages are saved to database*/}
+                {/* <div className="chatArea">
+                    {this.state.messages.map((message, index) => {
+                        return (
+                            <Card key={index}>
+                                <p>{message} lots of messages</p>
+                            </Card>
+                        )
+                    })}
+                </div> */}
+                <Form onSubmit={this.sendMessage}>
+                    <Form.Group> 
+                        <Form.Control
+                            type="text" 
+                            value={this.state.registerName}
+                            placeholder='Message'
+                            onChange={this.handleChange}>
+                        </Form.Control>
+                    </Form.Group> 
+                    <Button type="submit">Send</Button>
+                </Form>
             </div>
         )
     }
