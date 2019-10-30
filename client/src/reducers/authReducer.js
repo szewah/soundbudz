5dcc276047c44c8e0cb36a8b640097951e111295
szewah <swgrleong@gmail.com>
2019-10-30 14:59:22 -0400

    added init state

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,11 @@
+//set state
+const initState = {
+    isAuthenticated: false,
+    user: {},
+    loading: false
+};
+
 //the Reducer function takes a state and then an action
-const authReducer = (state, action) => {
+const authReducer = (state = initState, action) => {
     return state
 }
